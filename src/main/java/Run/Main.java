9d6e9c6620eb2a8b6a858684c40365027609b84f
v4resk <lucas.heurtin@gmail.com>
2020-12-14 23:12:47 +0100

    Distant bdd

--- src/main/java/Run/Main.java
+++ src/main/java/Run/Main.java
@@ -10,10 +10,10 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
 
-        final String url = "jdbc:mysql://localhost:3306/kidsLand";
-        final String passwd = "root";
-        final String user = "v4resk";
-
+        final String url = "jdbc:mysql://mysql-v4resk.alwaysdata.net:3306/v4resk_kidsland";
+        final String passwd = "MJJ6FxqA7vWpQxx";
+        final String user = "v4resk_user";
+    //MJJ6FxqA7vWpQxx
 
         try {
             Connection conn = DriverManager.getConnection(url,user,passwd);
